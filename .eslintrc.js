43154ac059747dfb651b0a2a33110408eec81a63
Matt Jarrett <cujarrett@gmail.com>
2018-04-13 18:07:56 -0500

    Adds initial files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,280 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
-  root: true,
-  parserOptions: {
-    parser: 'babel-eslint'
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true,
+    "jest": true
   },
-  env: {
-    browser: true,
+  "extends": [
+    "eslint:recommended",
+    "plugin:vue/recommended"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 8,
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true
+    },
+    "sourceType": "module"
   },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
-  ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
-  rules: {
-    // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  "rules": {
+    "array-callback-return": [
+      "error"
+    ],
+    "array-bracket-spacing": [
+        "error"
+    ],
+    "arrow-parens": [
+      "error",
+      "always"
+    ],
+    "arrow-spacing": [
+      "error",
+      {
+        "before": true,
+        "after": true
+      }
+    ],
+    "brace-style": [
+      "error",
+      "1tbs",
+      {
+        "allowSingleLine": true
+      }
+    ],
+    "camelcase": [
+      "error",
+      {
+        "properties": "always"
+      }
+    ],
+    "comma-dangle": [
+      "error",
+      "never"
+    ],
+    "comma-spacing": [
+      "error",
+      {
+        "before": false,
+        "after": true
+      }
+    ],
+    "comma-style": [
+      "error",
+      "last"
+    ],
+    "dot-notation": [
+      "error"
+    ],
+    "eol-last": [
+      "error"
+    ],
+    "eqeqeq": [
+      "error"
+    ],
+    "func-style": [
+      "error",
+      "expression"
+    ],
+    "generator-star-spacing": [
+      "error",
+      "after"
+    ],
+    "id-length": [
+      "error",
+      {
+        "min": 2
+      }
+    ],
+    "indent": [
+      "error",
+      2
+    ],
+    "keyword-spacing": [
+      "error",
+      {
+        "before": true,
+        "after": true
+      }
+    ],
+    "key-spacing": [
+      "error",
+      {
+        "beforeColon": false,
+        "afterColon": true
+      }
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "max-len": [
+      "error",
+      {
+        "code": 100
+      }
+    ],
+    "new-cap": [
+      "error",
+      {
+        "newIsCap": true,
+        "capIsNew": true
+      }
+    ],
+    "newline-per-chained-call": [
+      "error"
+    ],
+    "no-array-constructor": [
+      "error"
+    ],
+    "no-case-declarations": [
+      "error"
+    ],
+    "no-confusing-arrow": [
+      "error"
+    ],
+    "no-const-assign": [
+      "error"
+    ],
+    "no-dupe-class-members": [
+      "error"
+    ],
+    "no-iterator": [
+      "error"
+    ],
+    "no-loop-func": [
+      "error"
+    ],
+    "no-multiple-empty-lines": [
+      "error",
+      {
+        "max": 1,
+        "maxEOF": 1
+      }
+    ],
+    "no-multi-spaces": [
+      "error"
+    ],
+    "no-nested-ternary": [
+      "error"
+    ],
+    "no-new-func": [
+      "error"
+    ],
+    "no-new-object": [
+      "error"
+    ],
+    "no-param-reassign": [
+      "error"
+    ],
+    "no-undef": [
+      "error"
+    ],
+    "no-underscore-dangle": [
+      "error"
+    ],
+    "no-unneeded-ternary": [
+      "error"
+    ],
+    "no-unused-vars": [
+      "error"
+    ],
+    "no-useless-constructor": [
+      "error"
+    ],
+    "no-useless-escape": [
+      "error"
+    ],
+    "no-var": [
+      "error"
+    ],
+    "no-whitespace-before-property": [
+      "error"
+    ],
+    "one-var": [
+      "error",
+      "never"
+    ],
+    "object-curly-spacing": [
+      "error"
+    ],
+    "padded-blocks": [
+      "error",
+      "never"
+    ],
+    "prefer-arrow-callback": [
+      "error"
+    ],
+    "prefer-const": [
+      "error"
+    ],
+    "prefer-rest-params": [
+      "error"
+    ],
+    "prefer-spread": [
+      "error"
+    ],
+    "prefer-template": [
+      "error"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "radix": [
+      "error"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "space-before-blocks": [
+      "error",
+      "always"
+    ],
+    "space-before-function-paren": [
+      "error",
+      {
+        "named": "never",
+        "anonymous": "always",
+        "asyncArrow": "always"
+      }
+    ],
+    "space-in-parens": [
+      "error",
+      "never"
+    ],
+    "space-infix-ops": [
+      "error"
+    ],
+    "space-unary-ops": [
+      "error",
+      {
+        "words": true,
+        "nonwords": false
+      }
+    ],
+    "spaced-comment": [
+      "error",
+      "always"
+    ],
+    "template-curly-spacing": [
+      "error",
+      "never"
+    ],
+    "wrap-iife": [
+      "error",
+      "outside"
+    ],
+    "no-trailing-spaces": [
+      "error"
+    ],
+    // Project Overrides
+    "no-console": [
+      "off"
+    ],
+    "quote-props": [
+      "off"
+    ]
   }
 }
---
+++ README.md
@@ -0,0 +1,57 @@
+# Vue JS News App (_in development_)
+
+### What's it do?
+Builds a simple [Vue JS](https://vuejs.org/) app.
+
+## Setup
+Setup and use requires [Git](https://git-scm.com/),
+[Node JS (8.X.X or later)](https://nodejs.org/en/), and a text editor such as
+[VS Code](https://code.visualstudio.com/).
+
+If you're on a Mac, I'd suggest using [Homebrew](https://brew.sh/) for installing the required
+software listed in Setup.
+
+```bash
+git clone https://github.com/matt-jarrett/basic-vue-js-web-app.git
+cd basic-vue-js-web-app
+npm install
+```
+
+## Build Setup
+Install dependencies
+``` bash
+npm install
+```
+
+Lint code
+```bash
+npm run lint
+```
+
+Linting code and fix most errors
+```bash
+npm run lint-and-fix
+```
+
+Runs Continuous Integration (linting and tests)
+```bash
+npm run ci
+```
+
+Serve with hot reload at localhost:8080
+```bash
+npm run dev
+```
+
+Build for production with minification
+```bash
+npm run build
+npm start
+```
+
+Build for production and view the bundle analyzer report
+```bash
+npm run generate
+```
+
+For detailed explanation on how things work, checkout the [Nuxt.js docs](https://github.com/nuxt/nuxt.js).
