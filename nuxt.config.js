f4f7b37743401b88d74d78feb3fa2896f9261b65
Matt Jarrett <cujarrett@gmail.com>
2018-04-14 12:25:27 -0500

    Adds spacing in object-curly-spacing

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,18 +5,18 @@ module.exports = {
   head: {
     title: "vue-js-news-app",
     meta: [
-      {charset: "utf-8"},
-      {name: "viewport", content: "width=device-width, initial-scale=1"},
-      {hid: "description", name: "description", content: "Vue JS News App"}
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Vue JS News App" }
     ],
     link: [
-      {rel: "icon", type: "image/x-icon", href: "/favicon.ico"}
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
     ]
   },
   /*
   ** Customize the progress bar color
   */
-  loading: {color: "#3B8070"},
+  loading: { color: "#3B8070" },
   /*
   ** Build configuration
   */
@@ -29,7 +29,7 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend(config, {isDev, isClient}) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: "pre",
