f7658c81625ade84d6c9b960e2b40990c78f80b9
Matt Jarrett <cujarrett@gmail.com>
2018-05-25 10:21:11 -0500

    Adds global navigation with layouts and nuxt-link in Nuxt

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <header>
+      <nav class="flex w-75 justify-around f4 code pa1">
+        <nuxt-link to="/">Home</nuxt-link>
+        <nuxt-link to="/new">New Stories</nuxt-link>
+        <nuxt-link to="/show">Show Stories</nuxt-link>
+        <nuxt-link to="/ask">Ask Stories</nuxt-link>
+        <nuxt-link to="/jobs">Job Stories</nuxt-link>
+      </nav>
+    </header>
+    <nuxt/>
+  </div>
+</template>
