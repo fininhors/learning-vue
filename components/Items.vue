46d0ff4d78ef00b8dd58eca1cfacfefeddd813e9
Matt Jarrett <cujarrett@gmail.com>
2018-05-25 11:19:00 -0500

    Adds styling of the news list and news list items

--- components/Items.vue
+++ components/Items.vue
@@ -2,9 +2,21 @@
   <div class="code">
     <ul class="list pa2">
       <li
+        class="item"
         v-for="item in items"
         :key="item.id" >
-        {{ item.title }}
+        <div class="score">
+          {{ item.score }}
+        </div>
+        <div class="title">
+          {{ item.title }}
+        </div>
+        <div class="details">
+          by {{ item.by }} {{ item.time }}
+        </div>
+        <div class="comments">
+          {{ item.descendants }} comments
+        </div>
       </li>
     </ul>
   </div>
@@ -18,3 +30,36 @@ export default {
   ])
 }
 </script>
+
+<style scoped>
+  .item {
+    display: grid;
+    grid: repeat(4, 1.5em) / repeat(10, 1fr);
+    grid-row-gap: 1em;
+  }
+
+  .score {
+    grid-row: 1 / -1;
+    grid-column: span 1;
+    align-self: center;
+    justify-self: center;
+  }
+
+  .title {
+    grid-row: 1 / 3;
+    grid-column: 2 / -2;
+    align-self: end;
+  }
+
+  .comments {
+      grid-row: 3 / -1;
+      grid-column: 2 / 6;
+    }
+
+  .detials {
+    grid-row: 3 / -1;
+    grid-column: 6 / -2;
+    justify-self: end;
+  }
+
+</style>
