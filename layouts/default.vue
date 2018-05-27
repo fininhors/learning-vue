9e5893372ddc4b49f2304e6317dfeb595d70a437
Matt Jarrett <cujarrett@gmail.com>
2018-05-26 22:54:51 -0500

    Styles Navigation Based on Route

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,13 +2,19 @@
   <div>
     <header>
       <nav class="flex w-75 justify-around f4 code pa1">
-        <nuxt-link to="/">Home</nuxt-link>
-        <nuxt-link to="/new">New Stories</nuxt-link>
-        <nuxt-link to="/show">Show Stories</nuxt-link>
-        <nuxt-link to="/ask">Ask Stories</nuxt-link>
-        <nuxt-link to="/jobs">Job Stories</nuxt-link>
+        <nuxt-link class="link underline-hover" to="/">Home</nuxt-link>
+        <nuxt-link class="link underline-hover" to="/new">New Stories</nuxt-link>
+        <nuxt-link class="link underline-hover" to="/show">Show Stories</nuxt-link>
+        <nuxt-link class="link underline-hover" to="/ask">Ask Stories</nuxt-link>
+        <nuxt-link class="link underline-hover" to="/jobs">Job Stories</nuxt-link>
       </nav>
     </header>
     <nuxt/>
   </div>
 </template>
+<style>
+.nuxt-link-exact-active {
+  font-weight: bold;
+  text-decoration: underline;
+}
+</style>
