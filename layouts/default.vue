a469cec61d76faf493cce203327d033d14463eba
Matt Jarrett <cujarrett@gmail.com>
2018-05-28 10:54:58 -0500

    Simplifies headers

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,10 +3,10 @@
     <header>
       <nav class="flex w-75 justify-around f4 code pa1">
         <nuxt-link class="link underline-hover" to="/">Home</nuxt-link>
-        <nuxt-link class="link underline-hover" to="/new">New Stories</nuxt-link>
-        <nuxt-link class="link underline-hover" to="/show">Show Stories</nuxt-link>
-        <nuxt-link class="link underline-hover" to="/ask">Ask Stories</nuxt-link>
-        <nuxt-link class="link underline-hover" to="/jobs">Job Stories</nuxt-link>
+        <nuxt-link class="link underline-hover" to="/new">New</nuxt-link>
+        <nuxt-link class="link underline-hover" to="/show">Show</nuxt-link>
+        <nuxt-link class="link underline-hover" to="/ask">Ask</nuxt-link>
+        <nuxt-link class="link underline-hover" to="/jobs">Job</nuxt-link>
       </nav>
     </header>
     <nuxt/>
